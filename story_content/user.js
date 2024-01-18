988661a3220fc18ea2e147df3f5119ce183bfd9e
HodaBarghash <121548764+HodaBarghash@users.noreply.github.com>
2024-01-18 19:58:25 +0200

    Initial commit

---
+++ story_content/user.js
@@ -0,0 +1,27 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "65wUh9tJ6pv":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  const url="https://script.google.com/macros/s/AKfycbw0t7Yw13RrgBrLo3BN34FV55Vxufiy7hG_oSc67HZvWYl1EZaROZY9Y8LH7h0PwZjs/exec"
+
+var player=GetPlayer()
+
+fetch(url,{
+	method:"POST",
+	mode: 'no-cors',
+	cache:'no-cache',
+	headers: {'Content-Type': 'application/json'},
+	redirect:'follow',
+	body: JSON.stringify({v17q1attempts:player.GetVar("v17q1attempts"),v17q1answeredcorrect:player.GetVar("v17q1answeredcorrect")})
+	}
+	)
+}
+
